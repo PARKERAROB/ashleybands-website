f5abc248d5122cb8287d34346128c31fec0331a6
Atlas <308978577+atlas-ea@users.noreply.github.com>
2026-08-18 08:17:24 -0400

    Use trumpet icon for county instruments

--- app/portal/review/PortalSectionIcon.jsx
+++ app/portal/review/PortalSectionIcon.jsx
@@ -13,7 +13,7 @@ export default function PortalSectionIcon({ type }) {
 
   const art = {
     person: <><circle cx="12" cy="8" r="3.5" /><path d="M5 20c.7-4 3.1-6 7-6s6.3 2 7 6" /></>,
-    instrument: <><path d="M7 5h7v9.5a3.5 3.5 0 1 1-2-3.15V4l5-1v3" /><path d="M7 8h5" /></>,
+    instrument: <><path d="M3 10h8l7-4v12l-7-4H3Z" /><path d="M18 9h3M18 15h3M6 10V7M9 10V7M12 9V6" /><path d="M5 14v3h5" /></>,
     shirt: <><path d="M8 4 3.5 6.5 6 11l2-1v10h8V10l2 1 2.5-4.5L16 4c-.8 1.2-2.1 2-4 2S8.8 5.2 8 4Z" /></>,
     funding: <><circle cx="12" cy="12" r="9" /><path d="M15.5 8.5c-.8-.7-1.9-1-3.2-1-1.8 0-3 .9-3 2.2 0 3.2 6.1 1.7 6.1 4.8 0 1.3-1.3 2.2-3.2 2.2-1.5 0-2.8-.5-3.7-1.3M12 5.5v13" /></>
   };
